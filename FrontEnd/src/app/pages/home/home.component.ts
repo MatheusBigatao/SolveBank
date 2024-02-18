1b4a004d9542cbf95bc13261072014c693487c44
Hilton Rodrigo Mota <hrodrigomota@gmail.com>
2024-02-18 18:33:33 -0300

    create branch feature/home

--- FrontEnd/src/app/pages/home/home.component.ts
+++ FrontEnd/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuHomeComponent } from '../../components/menu-home/menu-home.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MenuHomeComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
