739332a6f132006d86ca7e3db0736a5f70439a6a
MarcusVogado <marcus_620@hotmail.com>
2024-02-21 19:03:45 -0300

    finalizada a tela de recuperação de senha

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { RecoverPasswordComponent } from './pages/recover-password/recover-password.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'recover-password', component: RecoverPasswordComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" }
+
 ]
