6182ca73b91b60cdceca7facc45cb24033287d42
matheusbigatao <matheus.bmartinelli@gmail.com>
2024-02-20 22:21:51 -0300

    feature: header diferente para telas pequenas

--- FrontEnd/src/app/components/menu-home/menu-home.component.ts
+++ FrontEnd/src/app/components/menu-home/menu-home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-menu-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu-home.component.html',
   styleUrl: './menu-home.component.css'
 })
 export class MenuHomeComponent {
+    isSmallScreen: boolean = window.innerWidth < 768;
 
+    constructor() {
+      this.checkScreenSize();
+    }
+  
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+      this.checkScreenSize();
+    }
+  
+    checkScreenSize() {
+      this.isSmallScreen = window.innerWidth < 768;
+    }
 }
