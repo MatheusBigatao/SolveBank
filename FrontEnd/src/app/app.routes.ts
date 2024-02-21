93edbc86856d23ff4de7e19e137de10a146cc45a
MarcusVogado <marcus_620@hotmail.com>
2024-02-21 19:12:43 -0300

    merge developer

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,14 +1,21 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { RecoverPasswordComponent } from './pages/recover-password/recover-password.component';
 
 export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
+<<<<<<< HEAD
   { path: 'home', component: HomeComponent },
   { path: 'recover-password', component: RecoverPasswordComponent },
   { path: "", redirectTo: "home", pathMatch: "full" }
 
 ]
+=======
+  { path: 'home', component: HomeComponent }
+]
+>>>>>>> f909386e5ad0279c15e702b220d23162fc8042e0
