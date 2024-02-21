d72b86d3ea77c579d44e0db3850d37d6c3a82437
Rafael Navarro <navarrorafaelb@gmail.com>
2024-02-20 21:53:48 -0300

    SplashScreen feita

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SplashscreenComponent } from './pages/splashscreen/splashscreen.component';
 
 export const routes: Routes = [
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'splashscreen', component: SplashscreenComponent}
 ];
