1235869e4a2941692167c99bf9e1c8021dafce5b
Hilton Rodrigo Mota <hrodrigomota@gmail.com>
2024-02-22 23:44:00 -0300

    review-code finished

---
+++ FrontEnd/src/app/components/menu-home-desktop/menu-home-desktop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-home-desktop',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-home-desktop.component.html',
+  styleUrl: './menu-home-desktop.component.css'
+})
+export class MenuHomeDesktopComponent {
+
+}
--- FrontEnd/src/app/tela-transacao/tela-transacao.component.ts
+++ FrontEnd/src/app/tela-transacao/tela-transacao.component.ts
@@ -1,16 +1,19 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { MenuHomeComponent } from '../components/menu-home/menu-home.component';
+import { MenuHomeDesktopComponent } from '../components/menu-home-desktop/menu-home-desktop.component';
 
 @Component({
   selector: 'app-tela-transacao',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NavbarComponent, MenuHomeComponent, MenuHomeDesktopComponent],
   templateUrl: './tela-transacao.component.html',
   styleUrl: './tela-transacao.component.css',
 })
 export class TelaTransacaoComponent {
-  isSmallScreen: boolean = window.innerWidth < 768;
+  isSmallScreen: boolean = window.innerWidth < 1120;
 
   constructor() {
     this.checkScreenSize();
@@ -22,6 +25,6 @@ export class TelaTransacaoComponent {
   }
 
   checkScreenSize() {
-    this.isSmallScreen = window.innerWidth < 768;
+    this.isSmallScreen = window.innerWidth < 1120;
   }
 }
