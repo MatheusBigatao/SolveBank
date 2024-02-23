9e042b0b61e0a4eadc367786098ce517ad51a5c8
MarcusVogado <marcus_620@hotmail.com>
2024-02-22 23:27:23 -0300

    update home-page

--- FrontEnd/src/app/pages/home/home.component.ts
+++ FrontEnd/src/app/pages/home/home.component.ts
@@ -12,9 +12,9 @@ import { CarouselModule } from 'primeng/carousel';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  
+  responsiveOptions: any[] | undefined;
   isSmallScreen: boolean = window.innerWidth < 1120;
-
+  cards: any[] = []
   constructor() {
     this.checkScreenSize();
   }
@@ -27,5 +27,52 @@ export class HomeComponent {
   checkScreenSize() {
     this.isSmallScreen = window.innerWidth < 1120;
   }
-} 
+
+  ngOnInit() {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1120px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 3,
+        numScroll: 1
+      }
+    ];
+    this.cards = [
+      {
+        title: "Pix",
+        image: "assets/Images/icon-pix.svg",
+        altImage: "Pix Icon"
+      },
+      {
+        title: "Transações",
+        image: "assets/Images/icon-transfer.svg",
+        altImage: "Transfer Icon"
+      },
+      {
+        title: "Pagar",
+        image: "assets/Images/icon-scan.svg",
+        altImage: "Scan Icon"
+      },
+      {       
+        title: "Comprovantes",
+        image: "assets/Images/icon-file-text.svg",
+        altImage: "File Text Icon"
+      }      
+
+    ]
+  }
+
+
+
+
+}
 
