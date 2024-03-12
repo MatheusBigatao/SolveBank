486b14ebd560a411f7336d6f6e5a4938295bd08c
MarcusVogado <marcus_620@hotmail.com>
2024-03-11 21:38:58 -0300

    update first commit backend

---
+++ BackEnd/WebApiSolveBank/SolveBank.Entities/DTOs/Usuario/CriarUsuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveBank.Entities.DTOs.Usuario
+{
+    public record CriarUsuario(string Nome, string Sobrenome)
+    {
+    }
+}
---
+++ BackEnd/WebApiSolveBank/SolveBank.Infrastructure/Configuration/SolveBankDbConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SolveBank.Entities.Models;
+
+namespace SolveBank.Infrastructure.Configuration
+{
+    public class SolveBankDbConfig:IdentityDbContext<Usuario>
+    {
+        public SolveBankDbConfig(DbContextOptions<SolveBankDbConfig> options) : base (options)
+        {
+            
+        }
+    }
+}
