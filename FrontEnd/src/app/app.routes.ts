a3120c0db19c26f6c4b4fe3ed5febe6fd17b0aed
Hilton Rodrigo Mota <hrodrigomota@gmail.com>
2024-03-14 11:02:09 -0300

    externalPages/home creation and adjust /home route component in the app.routes.ts file

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './pages/signUp/signUp.component';
-import { HomeComponent } from './pages/home/home.component';
+// import { HomeComponent } from './pages/home/home.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RecoverPasswordComponent } from './pages/recover-password/recover-password.component';
 import { SplashscreenComponent } from './pages/splashscreen/splashscreen.component';
 import { TransactionsComponent } from './pages/transactions/transactions.component';
+import { HomeComponent } from './externalPages/home/home.component';
+
 export const routes: Routes = [
   { path: '', component: SplashscreenComponent },
   { path: 'login', component: LoginComponent },
