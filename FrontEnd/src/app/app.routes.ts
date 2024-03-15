856c855e0539c9812cda9abec23944ca20f9ee33
Hilton Rodrigo Mota <hrodrigomota@gmail.com>
2024-03-15 11:15:17 -0300

    fix externalPages/home component and app.routes.ts file

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './pages/signUp/signUp.component';
-// import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RecoverPasswordComponent } from './pages/recover-password/recover-password.component';
 import { SplashscreenComponent } from './pages/splashscreen/splashscreen.component';
 import { TransactionsComponent } from './pages/transactions/transactions.component';
-import { HomeComponent } from './externalPages/home/home.component';
+import { ExternalHomeComponent } from './externalPages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: SplashscreenComponent },
@@ -16,4 +16,5 @@ export const routes: Routes = [
   { path: 'recover-password', component: RecoverPasswordComponent },
   { path: 'welcome', component: LandingPageComponent },
   { path: 'transactions', component: TransactionsComponent },
+  { path: 'external/home', component: ExternalHomeComponent },
 ];
--- FrontEnd/src/app/externalPages/home/home.component.ts
+++ FrontEnd/src/app/externalPages/home/home.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { MenuHomeComponent } from '../../components/menu-home/menu-home.component';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-external-home',
   standalone: true,
   imports: [MenuHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class ExternalHomeComponent {
 
 }
