d39385542d9c05861d9025942240689c5f8628e4
Julio Ramos <ima.julioramos@gmail.com>
2024-03-21 18:55:33 -0300

    Adicionando imports.

--- FrontEnd/src/app/externalPages/deposit/deposit.component.ts
+++ FrontEnd/src/app/externalPages/deposit/deposit.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-deposit',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule],
   templateUrl: './deposit.component.html',
-  styleUrl: './deposit.component.css'
+  styleUrl: './deposit.component.css',
 })
 export class ExternalDepositComponent {
+  agencia: string = ''; // Variáveis para armazenar os valores dos campos
+  conta: string = '';
+  valor: string = '';
 
+  constructor() {}
+
+  limparCampos(): void {
+    this.agencia = ''; // Atribui valores em branco para limpar os campos
+    this.conta = '';
+    this.valor = '';
+  }
 }
