e53285b96cf414937390787f02932f93b536c1c4
Julio Ramos <ima.julioramos@gmail.com>
2024-03-22 15:37:08 -0300

    Padronizando button

--- FrontEnd/src/app/externalPages/withdraw/withdraw.component.ts
+++ FrontEnd/src/app/externalPages/withdraw/withdraw.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-withdraw',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './withdraw.component.html',
-  styleUrl: './withdraw.component.css'
+  styleUrl: './withdraw.component.css',
 })
 export class ExternalWithdrawComponent {
-
+  value: string = '';
+  formulario: any;
+  limparCampos(): void {
+    this.value = '';
+  }
 }
