3ed4a95cfe132bbffcd9851d9da277bf7ed532bf
matheusbigatao <matheus.bmartinelli@gmail.com>
2024-03-24 02:16:28 -0300

    feature: serviço que irá fazer a requisição para sacar dinheiro da conta

--- FrontEnd/src/app/services/saque.service.ts
+++ FrontEnd/src/app/services/saque.service.ts
@@ -1,19 +1,28 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { uri } from './uriGlobalAcess';
+import { responseExibirUsuarioDTO } from '../models/DTOs/UsuarioDTOs/responseExibirUsuarioDTO';
 import { SaqueDTO } from '../models/DTOs/SaqueDTOs/SaqueDTO';
 @Injectable({
   providedIn: 'root'
 })
 export class SaqueService {
+    usuarioLogged: responseExibirUsuarioDTO = JSON.parse(localStorage.getItem("userLogged") || "") as responseExibirUsuarioDTO
 
-    constructor(private httpRequest:HttpClient) { }
+    constructor(private httpClient:HttpClient) { }
 
-    sacar(contaID: string, saqueDTO: SaqueDTO):Observable<any>{
-        return this.httpRequest.put<SaqueDTO>(`${uri}sacar/${contaID}`, saqueDTO).pipe(
+    sacar(saqueDTO: SaqueDTO):Observable<any>{
+        var contaID = this.usuarioLogged.contasBancarias[0].id;
+        return this.httpClient.put(`${uri}Saque/sacar/${contaID}`, saqueDTO, {
+            headers: new HttpHeaders({
+              'authorization': `Bearer ${this.usuarioLogged.webToken.token}`
+            })
+          }
+          ).pipe(
             res => res,
             error => error
           )
+
     }
 }
