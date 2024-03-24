ba245dddfa53eb619355ae3f2f567663eb10cdd7
matheusbigatao <matheus.bmartinelli@gmail.com>
2024-03-24 02:14:53 -0300

    Importando os componentes dos Alerts na página

--- FrontEnd/src/app/externalPages/withdraw/withdraw.component.ts
+++ FrontEnd/src/app/externalPages/withdraw/withdraw.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidationErrors, AbstractControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { SaqueService } from '../../services/saque.service';
+import { SaqueDTO } from '../../models/DTOs/SaqueDTOs/SaqueDTO';
+import { AlertCustom } from '../../models/Alert/AlertCustom';
+import { AlertErrorComponent } from '../../components/alert-error/alert-error.component';
+import { AlertSuccessComponent } from '../../components/alert-success/alert-success.component';
+import { LoadingSpinnerComponent } from '../../components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-withdraw',
   standalone: true,
-  imports: [RouterModule, FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    AlertErrorComponent,
+    AlertSuccessComponent,
+    LoadingSpinnerComponent
+],
   templateUrl: './withdraw.component.html',
   styleUrl: './withdraw.component.css',
 })
